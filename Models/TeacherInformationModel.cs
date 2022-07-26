60b3ee58e1b145c004f3056cc0b6f33d9240c235
cristiancast <cristiancast@unicauca.edu.co>
2022-07-26 16:41:29 -0500

    Implementando logica del profesor

---
+++ Models/TeacherInformationModel.cs
@@ -0,0 +1,8 @@
+namespace min_web_project_v2.Models
+{
+    public class TeacherInformationModel
+    {
+        public UserModel UserModel { get; set; }
+        public List<ScheduleModel> ScheduleModels { get; set; }
+    }
+}
